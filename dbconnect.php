b9b3f218bfffb6368928629ad235f369397cbc3d
aisha007 <51714913+aisha007@users.noreply.github.com>
2020-11-26 18:40:24 +0530

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -8,9 +8,9 @@
 
 	// Remote datatbase connection 
 	define('DB_SERVER' , 'remotemysql.com');
-	define('DB_USERNAME' , 'C39P2fOKVx');
-	define('DB_PASSWORD' , '2stQPfCAQI');
-	define('DB_NAME' , 'C39P2fOKVx');
+	define('DB_USERNAME' , '9o1uIzp1aw');
+	define('DB_PASSWORD' , 'Zp6xzVMQPi');
+	define('DB_NAME' , '9o1uIzp1aw');
 	
 	//connection with database
 	$conn = mysqli_connect(DB_SERVER , DB_USERNAME , DB_PASSWORD , DB_NAME);
